dddfccab04e8778384a393f0e664eebb1337ec46
Aryna Sylenko <aryna.sylenko@studenti.unitn.it>
2016-01-19 21:17:49 +0200

    fixed port

--- src/introsde/finalproject/processcentric/ServiceStandalone.java
+++ src/introsde/finalproject/processcentric/ServiceStandalone.java
@@ -10,7 +10,7 @@ public class ServiceStandalone {
     public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
         String PROTOCOL = "http://";
         String HOSTNAME = "0.0.0.0";
-        String PORT = "7000";
+        String PORT = "80";
         String BASE_URL = "/soap/processcentric";
         
         String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
